973a76cc19e8eea46e3bc3d2d8edc28bb1656e41
benhan82@gmail.com <benhan82@gmail.com@3a254ac9-263d-9d1d-d99d-403f98fda407>
2014-02-08 07:05:21 +0000

    found the fault with onTouchEvent, it only toggle for the object in which it is defined. Previously it was defined in the Activity so you had to touch the very outer edge to trigger touch event. Otherwise, touching within the borders of the ImageView meant that the touch even was dealt with by the View class.

--- trunk/src/com/benhan82/SOCK/views/HeadView.java
+++ trunk/src/com/benhan82/SOCK/views/HeadView.java
@@ -3,36 +3,25 @@ package com.benhan82.SOCK.views;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.graphics.Canvas;
-import android.graphics.Color;
 import android.graphics.Point;
 import android.view.Display;
-import android.view.View;
 import android.view.WindowManager;
+import android.widget.ImageView;
 
 import com.benhan82.SOCK.R;
 
-public class HeadView extends View {
+public class HeadView extends ImageView{
 
 	Bitmap head, scaledHead;
 	int bitX, bitY;
-	int canX, canY;
-	
+
 	public HeadView(Context context) {
 		super(context);
-		// Create bitmap object from image resource
 		head = BitmapFactory.decodeResource(getResources(), R.drawable.learning_head);
 		scaleHead(context);
-		
+		this.setImageBitmap(scaledHead);
 	}
 
-	@Override
-	protected void onDraw(Canvas canvas) {
-		super.onDraw(canvas);
-		canvas.drawColor(Color.GREEN);
-		canvas.drawBitmap(scaledHead, 0, 0, null);
-	}
-	
 	public void scaleHead(Context context) {
 		WindowManager wm = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
 		Display display = wm.getDefaultDisplay();
@@ -45,5 +34,5 @@ public class HeadView extends View {
 		float sf = (float) width / (float) bitX;
 		scaledHead = Bitmap.createScaledBitmap(head, width, (int) sf*bitY, false);
 	}
-
+	
 }
