0c8860353caf2223b15085ab0bf3120ccd5cb609
benhan82@gmail.com <benhan82@gmail.com@3a254ac9-263d-9d1d-d99d-403f98fda407>
2014-02-08 08:06:04 +0000

    The click and move touch gestures can be differentiated simply by looking at the event action property. An ACTION_UP event does not occur if there is movement, i.e. to slide the picture around

--- trunk/src/com/benhan82/SOCK/LearningHeadActivity.java
+++ trunk/src/com/benhan82/SOCK/LearningHeadActivity.java
@@ -1,15 +1,7 @@
 package com.benhan82.SOCK;
 
-import android.animation.ObjectAnimator;
-import android.animation.PropertyValuesHolder;
-import android.animation.ValueAnimator;
 import android.app.Activity;
 import android.os.Bundle;
-import android.support.v4.view.GestureDetectorCompat;
-import android.util.Log;
-import android.view.GestureDetector;
-import android.view.MotionEvent;
-import android.view.View;
 import android.widget.LinearLayout;
 
 import com.benhan82.SOCK.views.HeadView;
@@ -18,90 +10,15 @@ public class LearningHeadActivity extends Activity {
 	
 	LinearLayout container;
 	HeadView myView;
-    ObjectAnimator translateAnimation;
-    ObjectAnimator scaleAnimation;
-//    private GestureDetectorCompat mDetector; 
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_learning_head);
-
-//        mDetector = new GestureDetectorCompat(this, new MyGestureListener());
-        myView = new HeadView(this);
+		
+		myView = new HeadView(this);
         container = (LinearLayout) findViewById(R.id.container);
         container.addView(myView);
-
-        // Move the button over to the right and then back
-        translateAnimation = ObjectAnimator.ofFloat(myView, View.TRANSLATION_X, 800);
-        translateAnimation.setRepeatCount(1);
-        translateAnimation.setRepeatMode(ValueAnimator.REVERSE);
-        
-        // Scale the button in X and Y. Note the use of PropertyValuesHolder to animate
-        // multiple properties on the same object in parallel.
-        PropertyValuesHolder pvhX = PropertyValuesHolder.ofFloat(View.SCALE_X, 2);
-        PropertyValuesHolder pvhY = PropertyValuesHolder.ofFloat(View.SCALE_Y, 2);
-        scaleAnimation = ObjectAnimator.ofPropertyValuesHolder(myView, pvhX, pvhY);
-        scaleAnimation.setRepeatCount(1);
-        scaleAnimation.setRepeatMode(ValueAnimator.REVERSE);
-        
-//        myView.setOnTouchListener(new View.OnTouchListener() {
-//            @Override
-//            public boolean onTouch(View v, MotionEvent event) {
-//            	float x = event.getX();
-//            	float y = event.getY();
-//                if ( (x > 100) && (x < 400) && (y > 100) && (y < 400) ) {
-//                	translateAnimation.start();
-//                	return true;
-//                }
-//                else {
-//                	scaleAnimation.start();
-//                	return false;
-//                }
-//            }
-//        });
-
 	}
-
-    @Override 
-    public boolean onTouchEvent(MotionEvent event){ 
-    	float x = event.getX();
-    	float y = event.getY();
-        if ( (x > 100) && (x < 400) && (y > 100) && (y < 400) ) {
-        	Log.d("touch", "translateAnimation");
-        	translateAnimation.start();
-        }
-        else {
-        	Log.d("touch", "scaleAnimation");
-        	scaleAnimation.start();
-        }
-        return true;
-    }
-
-	// Subclass of SimpleOnGestureListener to respond only to single tap events
-//    class MyGestureListener extends GestureDetector.SimpleOnGestureListener {
-//
-//        @Override
-//        public boolean onDown(MotionEvent event) { 
-//            return true;
-//        }
-//        
-//        @Override
-//        public boolean onSingleTapUp(MotionEvent event) { 
-//
-//        	float x = event.getX();
-//        	float y = event.getY();
-//            if ( (x > 100) && (x < 400) && (y > 100) && (y < 400) ) {
-//            	Log.d("touch", "translateAnimation");
-//            	translateAnimation.start();
-//            }
-//            else {
-//            	Log.d("touch", "scaleAnimation");
-//            	scaleAnimation.start();
-//            }
-//            return true;
-//        }
-//    }
-	
 
 }
--- trunk/src/com/benhan82/SOCK/views/HeadView.java
+++ trunk/src/com/benhan82/SOCK/views/HeadView.java
@@ -1,10 +1,18 @@
 package com.benhan82.SOCK.views;
 
+import java.util.Calendar;
+
+import android.animation.ObjectAnimator;
+import android.animation.PropertyValuesHolder;
+import android.animation.ValueAnimator;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Point;
+import android.util.Log;
 import android.view.Display;
+import android.view.MotionEvent;
+import android.view.View;
 import android.view.WindowManager;
 import android.widget.ImageView;
 
@@ -14,12 +22,36 @@ public class HeadView extends ImageView{
 
 	Bitmap head, scaledHead;
 	int bitX, bitY;
+    ObjectAnimator translateAnimation;
+    ObjectAnimator scaleAnimation;
+//    private GestureDetectorCompat mDetector; 
+    
+    // Variables for onTouchEvent()
+    private float mDownX;
+    private float mDownY;
+    private final float SCROLL_THRESHOLD = 10;
+    private boolean isOnClick;
 
 	public HeadView(Context context) {
 		super(context);
 		head = BitmapFactory.decodeResource(getResources(), R.drawable.learning_head);
 		scaleHead(context);
 		this.setImageBitmap(scaledHead);
+
+        // Move the button over to the right and then back
+        translateAnimation = ObjectAnimator.ofFloat(this, View.TRANSLATION_X, 800);
+        translateAnimation.setRepeatCount(1);
+        translateAnimation.setRepeatMode(ValueAnimator.REVERSE);
+        
+        // Scale the button in X and Y. Note the use of PropertyValuesHolder to animate
+        // multiple properties on the same object in parallel.
+        PropertyValuesHolder pvhX = PropertyValuesHolder.ofFloat(View.SCALE_X, 2);
+        PropertyValuesHolder pvhY = PropertyValuesHolder.ofFloat(View.SCALE_Y, 2);
+        scaleAnimation = ObjectAnimator.ofPropertyValuesHolder(this, pvhX, pvhY);
+        scaleAnimation.setRepeatCount(1);
+        scaleAnimation.setRepeatMode(ValueAnimator.REVERSE);
+        
+//        mDetector = new GestureDetectorCompat(context, new MyGestureListener());
 	}
 
 	public void scaleHead(Context context) {
@@ -34,5 +66,78 @@ public class HeadView extends ImageView{
 		float sf = (float) width / (float) bitX;
 		scaledHead = Bitmap.createScaledBitmap(head, width, (int) sf*bitY, false);
 	}
+
+	@Override
+	public boolean onTouchEvent(MotionEvent event) {
+		
+	    switch (event.getAction()) {
+	        case MotionEvent.ACTION_UP: {
+                //click event has occurred
+            	Log.d("touch", "onClick()");
+            	
+	        	float x = event.getX();
+	        	float y = event.getY();
+	            if ( (x > 100) && (x < 400) && (y > 100) && (y < 400) ) {
+	            	Log.d("touch", "translateAnimation");
+	            	translateAnimation.start();
+	            }
+	            else {
+	            	Log.d("touch", "scaleAnimation");
+	            	scaleAnimation.start();
+	            }
+			}
+	        
+	    }
+
+    	Log.d("touch", "action: " + event.getAction());
+		return true;
+	}
+
+	// Subclass of SimpleOnGestureListener to respond only to single tap events
+//    class MyGestureListener extends GestureDetector.SimpleOnGestureListener {
+//
+//        @Override
+//        public boolean onDown(MotionEvent event) { 
+//        	Log.d("touch", "onDown()");
+//            return true;
+//        }
+//        
+//        @Override
+//		public void onLongPress(MotionEvent e) {
+//			// TODO Auto-generated method stub
+//        	Log.d("touch", "onLongPress()");
+//
+//        	float x = e.getX();
+//        	float y = e.getY();
+//            if ( (x > 100) && (x < 400) && (y > 100) && (y < 400) ) {
+//            	Log.d("touch", "translateAnimation");
+//            	translateAnimation.start();
+//            }
+//            else {
+//            	Log.d("touch", "scaleAnimation");
+//            	scaleAnimation.start();
+//            }
+//            
+//			super.onLongPress(e);
+//		}
+
+
+//		@Override
+//        public boolean onSingleTapUp(MotionEvent event) { 
+//
+//        	float x = event.getX();
+//        	float y = event.getY();
+//            if ( (x > 100) && (x < 400) && (y > 100) && (y < 400) ) {
+//            	Log.d("touch", "translateAnimation");
+//            	translateAnimation.start();
+//            }
+//            else {
+//            	Log.d("touch", "scaleAnimation");
+//            	scaleAnimation.start();
+//            }
+//            return true;
+//        }
+//	 }    
 	
 }
+
