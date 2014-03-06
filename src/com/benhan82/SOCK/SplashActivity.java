006108e63b96b7765ab5305fe165efcf6c1672b4
benhan82 <benhan82@gmail.com>
2014-03-06 16:07:57 +0000

    Added breakpoints to see when the database is created after application launch

--- src/com/benhan82/SOCK/SplashActivity.java
+++ src/com/benhan82/SOCK/SplashActivity.java
@@ -12,7 +12,7 @@ import android.view.Window;
 public class SplashActivity extends Activity {
 
 	MediaPlayer ourSong;
-	
+	private final int pauseDuration = 500;
 	@Override
 	protected void onCreate(Bundle savedInstanceState1) {
 		super.onCreate(savedInstanceState1);
@@ -25,19 +25,23 @@ public class SplashActivity extends Activity {
 		ourSong.start();
 
 		//Open the StartMenuActivity in a new thread
-		Thread timer = new Thread() {
-			public void run() {
-				try {
-					sleep(2000);
-				} catch (InterruptedException e) {
-					e.printStackTrace();
-				} finally {
-					Intent openMainActivity = new Intent("android.intent.action.MENU1");
-					startActivity(openMainActivity);
-				}
-			}
-		};
-		timer.start();
+//		Thread timer = new Thread() {
+//			public void run() {
+//				try {
+//					sleep(pauseDuration);
+//				} catch (InterruptedException e) {
+//					e.printStackTrace();
+//				} finally {
+//					Intent openMainActivity = new Intent("android.intent.action.MENU1");
+//					startActivity(openMainActivity);
+//					
+//				}
+//			}
+//		};
+//		timer.start();
+		
+		// for debugging this class will open the PatientSelectionActivity only
+		startActivity(new Intent(this, PatientSelectionActivity.class));
 	}
 
 	@Override
@@ -49,7 +53,7 @@ public class SplashActivity extends Activity {
 
 	@Override
 	protected void onPause() {
-		// when the activity goes to the background then end
+		// when the activity goes to the background, destroy this activity
 		super.onPause();
 		finish();
 	}
